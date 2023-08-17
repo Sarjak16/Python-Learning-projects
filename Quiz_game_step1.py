dec07ba4fb2f18a606cef3221da8b6702d7e6f6a
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2023-08-17 20:42:39 +0545

    All setup redy to play

--- Quiz_game_step1.py
+++ Quiz_game_step1.py
@@ -5,16 +5,20 @@ print(playing)
 if playing.lower() != "yes":
     quit()
 print("okay! lets start the game :)")
+score = 0
 
 answer = input(" What does CPU stand for? ").lower()
 if answer == "central processing unit":
     print("Correct answer!!!!!!!!")
+    score+= 1
+    
 else:
     print("Invalid answer!!!!")
     
 answer = input("fullform of GPU is? ").lower()
 if answer == "graphics processing unit":
     print("Correct answer!!!!!!!!")
+    score+= 1
 else:
     print("Invalid answer!!!!")
     
@@ -22,6 +26,7 @@ else:
     answer = input(" What does RAM stand for? ").lower()
 if answer == "random access memory":
     print("Correct answer!!!!!!!!")
+    score+= 1
 else:
     print("Invalid answer!!!!")
     
@@ -29,7 +34,10 @@ else:
     answer = input(" what does PSU stand for? ").lower()
 if answer == "power supply":
     print("Correct answer!!!!!!!!")
+    score+= 1
 else:
     print("Invalid answer!!!!")
     
     
+print("you got"+ str(score) +"questions correct")
+print("you got"+ str((score /4)*100) +"% 0f questions correct")
