eda66f4cc9d7daf6ef67622efd2bb65d3aaef5ea
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2023-08-22 20:23:31 +0545

    random number generation

--- Number_guesser.py
+++ Number_guesser.py
@@ -1,4 +1,21 @@
 import random
 
+top_of_range= input("Type a number: ") 
 
-print(random.randrange(-1, 10))
+
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+    
+    if top_of_range <= 0:
+        print("please type a num less than 0 next time.")
+        quit()
+else:
+    print("please type a number next time")
+    quit()
+        
+    
+    
+
+random_number= random.randint(0, top_of_range)
+print (random_number)
