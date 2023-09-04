4e8890258c3cae2b345e910a982b9a3a390803f2
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2023-09-04 19:12:02 +0545

    Update Rock_paper_scissors.py

--- Rock_paper_scissors.py
+++ Rock_paper_scissors.py
@@ -20,6 +20,18 @@ while True:
     
     if user_input=="rock" and computer_pick=="scissors":
         print("you won!")
-
+        user_wins += 1
+    elif user_input=="paper" and computer_pick=="rock":
+        print("you won!")
+        user_wins += 1
+    elif user_input=="scissors" and computer_pick=="paper":
+        print("you won!")
+        user_wins += 1
+        
+#     else:
+#         print("you lost!")
+#         computer_wins +=1
+# print("you won!", user_wins, "times" )
+# print("computer won!", computer_wins, "times")
 print ("Goodbye!")    
     
