f8c5fb996daf5de33902c3ad672c658213902b9a
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2023-09-10 22:03:12 +0545

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -2,4 +2,16 @@ import random
 import string
 
 def generate_password(min_length, numbers=True, special_characters=True):
-    pass
+    lettters= string.ascii_letters
+    digits= string.digits
+    special= string.punctuation
+    
+   characters= letters
+   if numbers:
+       characters += digits
+    if special_characters:
+        characters += special
+       
+    
+    
+generate_password(8)
