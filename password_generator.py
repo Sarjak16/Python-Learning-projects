ca7c557b2e217c4fb9ef4cd325a1ae6b74d61465
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2023-09-10 22:08:07 +0545

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -12,6 +12,19 @@ def generate_password(min_length, numbers=True, special_characters=True):
     if special_characters:
         characters += special
        
+    pwd= ""
+    meets_criteria= False
+    has_number= False
+    has_special= False
     
-    
+    while not meets_criteria or len(pwd)< min_length:
+        new_char= random.choice(characters)
+        pwd+= new_char
+        
+        if new_char in digits:
+            has_number= True
+        elif new_char in special:
+            has_special= True
+        
+        
 generate_password(8)
