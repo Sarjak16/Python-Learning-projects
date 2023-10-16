c930fc4c6ac3d62a7d025dc24f5060bff5a9a792
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2023-10-16 21:00:05 +0545

    Update myQR1.py

--- QR_code_Generator/myQR1.py
+++ QR_code_Generator/myQR1.py
@@ -6,5 +6,5 @@ features.add_data('http://www.youtube.com')
 features.make(fit=True)
 generate_image= features.make_image(fill_color="black", back_color="white")
 generate_image = qrcode.make("HELLO_WORLD")
-generate_image.save(image2.png)
+generate_image.save(image.png)
 
